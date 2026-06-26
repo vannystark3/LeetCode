65bacb69be066b48bdd41a7378141c6d971479fa
Aditya Khurana <151136783+vannystark3@users.noreply.github.com>
2026-06-26 17:38:11 +0530

    Time: 3 ms (85.49%), Space: 21 MB (86.99%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        votes = 0
+        for num in nums:
+            if votes==0:
+                candidate=num
+            if candidate==num:
+                votes += 1
+            else:
+                votes -= 1
+        return candidate
