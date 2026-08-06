e61065364c0421050230e52119fedc8df25f5d7a
Aditya Khurana <151136783+vannystark3@users.noreply.github.com>
2026-08-07 03:28:45 +0530

    Time: 191 ms (94.98%), Space: 19.4 MB (35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,13 +1,20 @@
-class Solution(object):
-    def longestPalindrome(self, s):
+class Solution:
+    def checkPallindrom(self,s,low,high,l):
+        while low>=0 and high<l and s[low]==s[high]:
+            low-=1
+            high+=1
+        return s[low+1:high]
+    def longestPalindrome(self, s: str) -> str:
         l = len(s)
-        c = 1
-        ans = s[0]
-        for i in range(l):
-            st = s[i]
-            for j in range(i+1,l):
-                st += s[j]
-                if st==st[::-1] and len(st)>c:
-                    c = len(st)
-                    ans = st
-        return ans                        
+        maxi = s[0]
+        if l==1:
+            return s
+        for i in range(l-1):
+            odd = self.checkPallindrom(s,i,i,l)
+            even = self.checkPallindrom(s,i,i+1,l)
+            if len(odd)>len(maxi):
+                maxi = odd
+            if len(even)>len(maxi):
+                maxi = even
+        return maxi
+
