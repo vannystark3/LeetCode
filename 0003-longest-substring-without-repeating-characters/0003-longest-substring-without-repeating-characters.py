9471c3eee901df882a4ff20fc6bef460890c1c7b
Aditya Khurana <151136783+vannystark3@users.noreply.github.com>
2026-08-13 03:55:49 +0530

    Time: 187 ms (27.96%), Space: 19.8 MB (20.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,11 +3,12 @@ class Solution:
         maxi=0
         l = len(s)
         left = 0
-        d = set()
+        d = {}
         for right in range(l):
-            while s[right] in d:
-                d.remove(s[left])
-                left+=1
-            d.add(s[right])
+            if s[right] not in d or d[s[right]]<left:
+                d[s[right]] = right
+            else:
+                left = d[s[right]]+1
+                d[s[right]] = right
             maxi = max(maxi,right-left+1)
         return maxi
